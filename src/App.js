0bf3158c4c8167b1206e22830777f08c16b13d8b
kroles1 <100869848+kroles1@users.noreply.github.com>
2022-11-09 10:02:22 +0000

    navbar and searchfomr added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Search, User, Repo } from './pages';
+import {NavBar} from './components'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path='/' element={<Search/>}>
           <Route path=':user' element={<User/>}>
